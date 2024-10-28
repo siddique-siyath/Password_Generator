fe2ebd34a0026d3017a1a397ca1496981519fea3
siddique-siyath <siddiquesiyath39@gmail.com>
2024-10-29 00:54:25 +0530

    logic update

--- password_generator/src/app/form/form.component.ts
+++ password_generator/src/app/form/form.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent implements OnInit{
+
+  password:any;
+  passwordLength:number = 15;
+  checkBox = {
+    capitalLetter:true,
+    smallLetter:true,
+    number:true,
+    symbol:true
+  }
+  passwordType:string = "Very Strong"
+
+   uppercase:string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     lowercase:string = "abcdefghijklmnopqrstuvwxyz";
+     numbers:string = "0123456789";
+     symbols:string = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  generatePassword(){
+    let characters = "";
+
+    if(this.checkBox.capitalLetter) characters += this.uppercase
+    if(this.checkBox.smallLetter) characters += this.lowercase
+    if(this.checkBox.number) characters += this.numbers
+    if(this.checkBox.symbol) characters += this.symbols
+
+    if(this.passwordLength <= 4) this.passwordType = "Very Week"
+    if(this.passwordLength >= 5 && this.passwordLength <= 7) this.passwordType = "Week"
+    if(this.passwordLength >= 8 && this.passwordLength <= 9) this.passwordType = "Good"
+    if(this.passwordLength >= 10 && this.passwordLength <= 11) this.passwordType = "Strong"
+    if(this.passwordLength >= 11) this.passwordType = "Very Strong"
+
+
+    let password = "";
+    let length = this.passwordLength
+    
+    if (characters === "") {
+      this.password = "";
+      return ;
+  }
+  
+    // Generate password by randomly selecting characters
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    this.password = password;
+    console.log(password)
+  }
 
 }
